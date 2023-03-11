7a5dae52abc469841a66143bc9e3c55b83a177c3
Maksim.K <omega.kseno@gmail.com>
2023-03-11 23:39:32 +0300

    Add Task04

--- Task_04.py
+++ Task_04.py
@@ -5,3 +5,18 @@
 #6 -> 1  4  1
 #24 -> 4  16  4
 #60 -> 10  40  10
+
+number = int(input('введите число ->'))
+if number%6==0 and number>6:
+   sum = int(number/3)
+   guis=int(sum/2)
+   girl = int(guiss*4)
+   print(number,'сделали->',guis,girl,guis)
+elif number < 6:
+   print('вы ввели маленькое число,введите число больше 6')
+else:    
+    temp = (number//6)*6
+    sum = int(temp/3)
+    guis=int(sum/2)
+    girl = int(guis*4)
+    print(temp,'сделали ->',guis,girl,guis,'осталось->',number-temp)

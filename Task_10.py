de2184f866ca701d3074fe2cd8b77967054ac47e
Maksim.K <omega.kseno@gmail.com>
2023-03-14 00:18:10 +0300

    добавил проверку Task10

--- Task_10.py
+++ Task_10.py
@@ -2,21 +2,27 @@
 #Определите минимальное число монеток, которые нужно перевернуть, чтобы все монетки были повернуты вверх одной и той же стороной. 
 #Выведите минимальное количество монет, которые нужно перевернуть.
 from random import randint
-
-numberOfCoins = int(input('введите количество монет ->')) 
-tails = 0
-eagle = 0
-for i in  range(numberOfCoins): 
-    coin =   randint(0,1)
-    print(coin,end = " ") 
-    if coin == 1:
-        tails = tails+1
+temp = 1
+while temp:
+    numberOfCoins = input('введите количество монет ->')
+    if numberOfCoins.isdigit():
+        numberOfCoins =  int(numberOfCoins)
+        tails = 0
+        eagle = 0
+        for i in  range(numberOfCoins): 
+            coin =   randint(0,1)
+            print(coin,end = " ") 
+            if coin == 1:
+                tails = tails+1
+            else:
+                eagle = eagle+1    
+        if eagle<tails:
+            print(f"минимальное количество монет которые нужно перевернуть -> {eagle} ")
+        else:
+            print(f"минимальное количество монет которые нужно перевернуть -> {tails}")
+        temp=0
     else:
-        eagle = eagle+1    
-if eagle<tails:
-    print(f"минимальное количество монет которые нужно перевернуть -> {eagle} ")
-else:
-    print(f"минимальное количество монет которые нужно перевернуть -> {tails}")    
+        print('Введите целое число')            
 
 
 
